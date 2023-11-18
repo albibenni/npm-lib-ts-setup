568560646b7fef58ac2d388259ed574b95ac2974
Alberto Benatti <albi.benni8@gmail.com>
2023-11-18 17:57:29 +0100

    add tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig(async () => {
+  return [
+    {
+      entry: ["src/**/*.ts"],
+      format: ["esm", "cjs"],
+      dts: true,
+    },
+  ];
+});
