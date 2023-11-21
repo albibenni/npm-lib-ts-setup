b030a37e561052ea4321b1f633b126d12e50580b
Alberto Benatti <albi.benni8@gmail.com>
2023-11-21 20:55:41 +0100

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    extends: [
-        "eslint:recommended",
-        "prettier"
-    ]
-}
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "prettier"],
+};
 
+// eslint-disable-next-line no-undef
 module.exports = config;
